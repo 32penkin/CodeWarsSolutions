ea1cfdf7eca79203df29674cbeb788062e3ecd34
Yauhen <penkin.evgeniy@gmail.com>
2017-03-20 08:51:57 +0300

    Hello

---
+++ Opposite number.js
@@ -0,0 +1,7 @@
+/*Description:
+  Very simple, given a number, find its opposite.
+But can you do it in 1 line of code and without any conditionals?*/
+
+function opposite(number) {
+  return Number(-number);
+}
